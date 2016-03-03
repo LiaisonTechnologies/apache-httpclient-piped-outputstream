cc0703a93b7a2491153ee93b8ed7e3de4f5f59be
Robert Christian <Rob@Roberts-MacBook-Pro.local>
2016-03-02 18:35:24 -0800

    refactor/cleanu

--- src/main/java/com/liaison/transports/http/client/ACLPipedOutputStream.java
+++ src/main/java/com/liaison/transports/http/client/ACLPipedOutputStream.java
@@ -1,6 +1,12 @@
 package com.liaison.transports.http.client;
 
+import io.dropwizard.lifecycle.ExecutorServiceManager;
+import io.dropwizard.lifecycle.setup.ExecutorServiceBuilder;
+
 import java.io.PipedOutputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by Rob on 3/2/16.
@@ -10,8 +16,14 @@ public class ACLPipedOutputStream extends PipedOutputStream {
 
     public ACLPipedOutputStream(String url) throws Exception {
         ApacheRequestExecutor requestExecutor = new ApacheRequestExecutor(this, url);
-        new Thread(requestExecutor).start(); // TODO move into write for performance (minimizes thread alive time)
+
+        // TODO properly vet pooling strategy
+        // TODO should be registered with ExecutorServiceManager
+        ExecutorService es = Executors.newCachedThreadPool();
+        es.execute(requestExecutor); // TODO move this into first byte written for optimization
+        es.shutdown();
     }
+
 
 
 }
--- src/test/java/com/liaison/transports/http/client/NonBlockingTest.java
+++ src/test/java/com/liaison/transports/http/client/NonBlockingTest.java
@@ -7,7 +7,6 @@ import java.io.OutputStream;
  */
 public class NonBlockingTest {
 
-
     public static void main(String[] args) throws Exception {
 
         // get output stream for an endpoint
@@ -20,5 +19,12 @@ public class NonBlockingTest {
         os.close();
 
     }
+
+  /*  1. Are we heading into a bad zone here with too many threads being spawned and managed?
+
+      2. We need to simulate the case where the WriteToSocket() is slow — >> the PipeInputStream.read() will be slow — > begs the question:  does it stall the writer ? Or does the buffer of the pipe blow up?
+
+      3. We should test performance of Pipes under load + under concurrency + under slow PipeInputStream read to make sure we don’t have overflow pipes or broken pipes etc..
+   */
 
 }
